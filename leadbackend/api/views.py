a640aee1a770db3ab3441a5c735a629f35429543
github-ashwin <ashi.github@gmail.com>
2025-07-06 11:56:12 +0530

    Sample Views - GET api

--- leadbackend/api/views.py
+++ leadbackend/api/views.py
@@ -20,4 +20,44 @@ class GoodMorningView(APIView):
 
         return Response(data=context)
 
+class GoodAfternoonView(APIView):
+
+    def get(self,request,*args, **kwargs):
+
+        context = {"message":"Good Afternoon!"} # Python Dictionary
+
+        # Response class converts python native type to json
+
+        return Response(data=context)
+    
+class GoodAfternoonView(APIView):
+
+    def get(self,request,*args, **kwargs):
+
+        context = {"message":"Good Afternoon!"} # Python Dictionary
+
+        # Response class converts python native type to json
+
+        return Response(data=context)
+    
+class GoodEveningView(APIView):
+
+    def get(self,request,*args, **kwargs):
+
+        context = {"message":"Good Evening!"} # Python Dictionary
+
+        # Response class converts python native type to json
+
+        return Response(data=context)
+    
+class GoodNightView(APIView):
+
+    def get(self,request,*args, **kwargs):
+
+        context = {"message":"Good Night!"} # Python Dictionary
+
+        # Response class converts python native type to json
+
+        return Response(data=context)
+
 
