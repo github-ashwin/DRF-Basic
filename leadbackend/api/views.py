6fa1744fafdfad885d25e1d789b4d537576a3b8a
github-ashwin <ashi.github@gmail.com>
2025-07-06 11:47:33 +0530

    Adding Sample GET method

--- leadbackend/api/views.py
+++ leadbackend/api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+"""
+HTTPRequest
+URL: localhost:8000/morning/
+Method: GET
+Response: Good Morning!
+"""
+
+class GoodMorningView(APIView):
+
+    def get(self,request,*args, **kwargs):
+
+        context = {"message":"Good Morning!"} # Python Dictionary
+
+        # Response class converts python native type to json
+
+        return Response(data=context)
+
+
